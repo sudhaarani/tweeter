8bce40f3bd23f03d04d490694dbcc5bfca35bf9c
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-11-18 19:33:05 -0800

    slide the whole new-tweet section up or down when the 'write a new tweet' button is clicked. When thetweet-form appears (slides down), the user has to then click on the text area to begin typing and also move new tweet arrow up and down so that it is clear to the user that it is a clickable link

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,38 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-//client-side js logic goes here
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
 
 const createTweetArticle = (tweet) => {
   const $article = $('<article>');
@@ -49,6 +17,7 @@ const createTweetArticle = (tweet) => {
 
   $header.append($avatarName).append($handle);
 
+  //to prevent XSS from user through textarea,using .text() Escaping method
   const $tweetContent = $('<div>').append($('<b>')).text(tweet.content.text);
 
   const $footer = $('<footer>').addClass('footer-article')
@@ -83,56 +52,72 @@ const renderUserTweet = function (tweets) {
 
 $(document).ready(function () {
   console.log("ready");
-
   $(function () {
-    const $form = $("#tweet-form");
-    console.log("form:", $form);
-    $form.on("submit", function (event) {
-      event.preventDefault();//to prevent data(form) submission and page refresh
-      $tweet = $("#tweet-text").serialize();//turns a set of form data into a query string
-      console.log("tweet:", $tweet);
-      $("#error-display").slideUp();
-      if ($("#tweet-text").val() === "" || null) {
-        $("#error-display").text("Please enter a tweet");
-        return $("#error-display").slideDown();
-      }
-      console.log("length:", $("#tweet-text").val().length);
-      if ($("#tweet-text").val().length > 140) {
-        $("#error-display").text("Your message length exceeds 140 character!");
-        return $("#error-display").show();
-      }
 
-      //to submit a POST request that sends the serialized data to the server
-      $.ajax({
-        method: 'POST',
-        url: '/tweets',
-        data: $tweet,
-        success: function (res) {
-          $('#tweet-text').val('');//clearing the textarea once msg is sent
-          $(".counter").val("140").css({ "font-weight": "bold", "color": "black" });//set counter back to 140 count with added css once msg is sent
-          console.log(`data received:: ${res}`);
-        },
-        error: function (err) {
-          console.log(`error received:: ${err}`);
+    //to move new tweet up and down
+    function topInc() {
+      $("#new-tweetArrow").animate({ top: '10px' }, 500, topDec); //topDec is callback func which s added to move infinitively
+    }
+    function topDec() {
+      $("#new-tweetArrow").animate({ top: '0px' }, 500, topInc);
+    }
+    topInc();
+
+    //to slide the whole new-tweet section down when the button is clicked
+    $("#new-tweetBtn").on("click", function () {
+      $(".new-tweet").slideDown();
+
+      const $form = $("#tweet-form");
+      $form.on("submit", function (event) {
+        event.preventDefault();//to prevent data(form) submission and page refresh
+        $tweet = $("#tweet-text").serialize();//turns a set of form data into a query string
+
+        $("#error-display").slideUp(); //div with this id is hidden when page is loaded
+
+        if ($("#tweet-text").val() === "" || null) {//to show err msg when text area is empty
+          $("#error-display").text("Please enter a tweet");
+          return $("#error-display").slideDown();//div with this id is shown when text area is empty and msg is shown
         }
-      });
+        if ($("#tweet-text").val().length > 140) {//to show err msg when text area is exceeds 140 char
+          $("#error-display").text("Your message length exceeds 140 character!");
+          return $("#error-display").slideDown();
+        }
 
-      //to make a request to /tweets and receive the array of tweets as JSON
-      const loadTweets = function () {
+        //to submit a POST request that sends the serialized data to the server
         $.ajax({
-          url: "/tweets",
-          method: "GET",
-          dataType: "json",
-          success: (jsondata) => { //data received from server
-            console.log("data received json:", jsondata);
-            renderUserTweet(jsondata);
+          method: 'POST',
+          url: '/tweets',
+          data: $tweet,
+          success: function (res) {
+            $('#tweet-text').val('');//clearing the textarea once msg is sent
+            $(".counter").val("140").css({ "font-weight": "bold", "color": "black" });//set counter back to 140 count with added css once msg is sent
+            console.log(`data received:: ${res}`);
           },
-          error: (error) => {
-            console.log("error received:", error);
-          },
+          error: function (err) {
+            console.log(`error received:: ${err}`);
+          }
         });
-      };
-      loadTweets();
+        loadTweets();//to load entered tweet as an article
+      });
     });
+
+    //to make a request to /tweets and receive the array of tweets as JSON
+    const loadTweets = function () {
+      console.log("loadTweets func:");
+      $.ajax({
+        url: "/tweets",
+        method: "GET",
+        dataType: "json",
+        success: (jsondata) => { //data received from server
+          console.log("data received json:", jsondata);
+          renderUserTweet(jsondata);
+        },
+        error: (error) => {
+          console.log("error received:", error);
+        },
+      });
+    };
+    loadTweets();//to load tweets in db as an article
+
   });
 });

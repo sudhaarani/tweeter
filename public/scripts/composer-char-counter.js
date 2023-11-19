32b156c18c2d271dda9cd596759b62f39db89404
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-11-19 03:13:34 -0800

    Make the page scroll to the top and enable the textarea when the(new) up-arrow button is clicked

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,4 +10,26 @@ $(document).ready(function () {
     }
     parentToChild.html(`<b>${lettersRemaining}</b>`);//.html or.text can be used to update the text
   });
+
+  //scroll to top of the page
+  // Make the page scroll to the top and enable the textarea when the(new) up-arrow button is clicked
+  $(window).on("scroll", function () {
+    // When the user scrolls down 20px from the top of the document, show the button
+    if ($(document).scrollTop() > 20) {
+      //   console.log("scrollTop() > 20");
+      $("#scrollBtn").css({ "display": "block" });
+    } else {
+      //  console.log("scrollTop() < 20");
+      $("#scrollBtn").css({ "display": "none" });
+    }
+    // $(".logo").css({ "position": "fixed" });
+
+    // When the user clicks on the button, scroll to the top of the document
+    $("#scrollBtn").on("click", function () {
+      // console.log("btn clicked");
+      $(document).animate({ scrollTop: 0 });
+      $(".new-tweet").show();//to show the new-tweet section from hidden
+      $("#tweet-text").focus(); //to enable the textarea
+    });
+  });
 });

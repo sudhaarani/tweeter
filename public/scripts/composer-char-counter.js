06b27669c4ff0a84bd8f20bb63537dfb88e7d7d8
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-11-20 00:58:37 -0800

    change the code to display the counter valuebased on textarea length so that counter value gets updated correctly even when u delete a char in the textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,13 @@ $(document).ready(function () {
   $("#tweet-text").on("input", function () {
     //to traverse up the DOM tree from that node/element and then back down to a node that matches the '.counter'
     const parentToChild = $(this).parents("form").find(".counter");//"this" points to the textarea.
-    const lettersRemaining = parentToChild.val() - 1;
+    const textarea = $(this).parents("form").find("#tweet-text");
+    let lettersRemaining = 140 - textarea.val().length;
     if (lettersRemaining < 0) {
       parentToChild.css({ "color": "red" });
+    }
+    if (lettersRemaining >= 0) {
+      parentToChild.css({ "color": "black" });
     }
     parentToChild.html(`<b>${lettersRemaining}</b>`);//.html or.text can be used to update the text
   });
@@ -16,18 +20,14 @@ $(document).ready(function () {
   $(window).on("scroll", function () {
     // When the user scrolls down 20px from the top of the document, show the button
     if ($(document).scrollTop() > 20) {
-      //   console.log("scrollTop() > 20");
       $("#scrollBtn").css({ "display": "block" });
     } else {
-      //  console.log("scrollTop() < 20");
       $("#scrollBtn").css({ "display": "none" });
     }
-    // $(".logo").css({ "position": "fixed" });
 
     // When the user clicks on the button, scroll to the top of the document
     $("#scrollBtn").on("click", function () {
-      // console.log("btn clicked");
-      $(document).animate({ scrollTop: 0 });
+      $('html, body').animate({ scrollTop: 0 }, 100);
       $(".new-tweet").show();//to show the new-tweet section from hidden
       $("#tweet-text").focus(); //to enable the textarea
     });
